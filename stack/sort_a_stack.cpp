83146a59846b48644558913d1d47d3a3c1fbfe7b
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-06-25 20:10:47 +0530

    Solved Sort a Stack

---
+++ stack/sort_a_stack.cpp
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+class SortedStack {
+   public:
+    stack<int> s;
+    void sort();
+};
+
+void printStack(stack<int> s) {
+    while (!s.empty()) {
+        printf("%d ", s.top());
+        s.pop();
+    }
+    printf("\n");
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        SortedStack *ss = new SortedStack();
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            int k;
+            cin >> k;
+            ss->s.push(k);
+        }
+        ss->sort();
+        printStack(ss->s);
+    }
+}  // } Driver Code Ends
+
+/*The structure of the class is
+class SortedStack{
+public:
+	stack<int> s;
+	void sort();
+};
+*/
+
+/* The below method sorts the stack s 
+you are required to complete the below method */
+void SortedStack ::sort() {
+    //Your code here
+    stack<int> aux;
+    int n = s.size();
+    while (n--) {
+        while (!s.empty()) {
+            int t = s.top();
+            s.pop();
+            if (s.empty()) {
+                aux.push(t);
+            } else if (t >= s.top()) {
+                aux.push(t);
+            } else {
+                aux.push(s.top());
+                s.pop();
+                s.push(t);
+            }
+            // cout << "Reached Here " << s.size() << " " << aux.size() << endl;
+        }
+
+        while (!aux.empty()) {
+            s.push(aux.top());
+            aux.pop();
+        }
+    }
+}
