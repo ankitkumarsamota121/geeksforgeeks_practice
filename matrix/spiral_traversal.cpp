e9b0e16c70b5f11e8379dc8b677bb2ed3d5b0f6a
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-07-05 22:02:56 +0530

    Solved Spiral Traversal of a Matrix

---
+++ matrix/spiral_traversal.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, m, n;
+    int mat[10][10];
+    cin >> t;
+    while (t--) {
+        cin >> m >> n;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                cin >> mat[r][c];
+            }
+        }
+
+        int r = 0, c = 0;
+        int r_start = 0, r_end = m - 1;
+        int c_start = 0, c_end = n - 1;
+
+        while (r_start <= r_end and c_start <= c_end) {
+            // 1
+            // r = r_start;
+            // c = c_start;
+
+            while (c <= c_end and r <= r_end) {
+                cout << mat[r][c] << ' ';
+                c++;
+            }
+            c--;
+            r++;
+
+            // 2
+            while (r <= r_end and c >= c_start) {
+                cout << mat[r][c] << ' ';
+                r++;
+            }
+            c--;
+            r--;
+
+            // 3
+            while (c >= c_start and r > r_start) {
+                cout << mat[r][c] << ' ';
+                c--;
+            }
+            c++;
+            r--;
+
+            // 4
+            while (r > r_start and c < c_end) {
+                cout << mat[r][c] << ' ';
+                r--;
+            }
+            c++;
+            r++;
+
+            // cout << endl;
+
+            r_start++;
+            c_start++;
+
+            r_end--;
+            c_end--;
+        }
+        cout << endl;
+    }
+    return 0;
+}
