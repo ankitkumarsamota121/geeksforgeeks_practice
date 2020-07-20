6813b4bb389bb8af67e0e062696d93e08c6b70b7
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-07-20 17:10:23 +0530

    Solved Maximum difference of node and ancestor

---
+++ trees/min_dist_between_two_nodes.cpp
@@ -0,0 +1,129 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int findDist(Node *, int, int);
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        int n1, n2;
+        scanf("%d %d ", &n1, &n2);
+        cout << findDist(root, n1, n2) << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
+
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+bool findPath(Node *root, int a, vector<Node *> &path) {
+    if (!root)
+        return false;
+
+    path.push_back(root);
+    if (root->data == a)
+        return true;
+
+    bool x = findPath(root->left, a, path);
+    bool y = findPath(root->right, a, path);
+    if (!x && !y) {
+        path.pop_back();
+        return false;
+    }
+    return true;
+}
+
+/* Should return minimum distance between a and b
+   in a tree with given root*/
+int findDist(Node *root, int a, int b) {
+    // Your code here
+    vector<Node *> pathA, pathB;
+    findPath(root, a, pathA);
+    findPath(root, b, pathB);
+
+    int i = 0;
+    while (i < pathA.size() && i < pathB.size() && pathA[i] == pathB[i]) {
+        i++;
+    }
+
+    return (pathA.size() - i) + (pathB.size() - i);
+}
---
+++ trees/serialize_deserialize.cpp
@@ -0,0 +1,156 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current Node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+            // Create the right child for the current Node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// } Driver Code Ends
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child  
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+
+/*this  function serializes 
+the binary tree and stores 
+it in the vector A*/
+void serialize(Node *root, vector<int> &A) {
+    //Your code here
+    queue<Node *> Q;
+    Q.push(root);
+    while (!Q.empty()) {
+        Node *t = Q.front();
+        Q.pop();
+
+        if (!t)
+            A.push_back(-1);
+        else
+            A.push_back(t->data);
+
+        if (t) {
+            Q.push(t->left);
+            Q.push(t->right);
+        }
+    }
+}
+
+Node *deUtil(vector<int> &A, int idx) {
+    if (A[idx] == -1 || idx >= A.size())
+        return NULL;
+
+    Node *root = new Node(A[idx]);
+    root->left = deUtil(A, 2 * idx + 1);
+    root->right = deUtil(A, 2 * idx + 2);
+    return root;
+}
+
+/*this function deserializes
+ the serialized vector A*/
+Node *deSerialize(vector<int> &A) {
+    //Your code here
+    Node *root = deUtil(A, 0);
+    return root;
+}
+
+// { Driver Code Starts.
+
+void inorder(Node *root) {
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        vector<int> A;
+        serialize(root, A);
+
+        Node *getRoot = deSerialize(A);
+        inorder(getRoot);
+        cout << "\n";
+    }
+
+    return 0;
+}  // } Driver Code Ends
