4acd44f9cf5b629a4aa1333276fdf383b1cd7ef8
Ankit Samota <ankitkumarsamota121@gmail.com>
2020-12-31 11:37:09 +0530

    Solved Sort an array of 0 1 2

---
+++ placement_series/arrays/sort012.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+void sort012(int[], int);
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        sort012(a, n);
+
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+void sort012(int a[], int n) {
+    // coode here
+    int i = 0, j = 0, k = n - 1;
+    while (j <= k) {
+        if (a[j] == 0) {
+            swap(a[i], a[j]);
+            i++;
+            j++;
+        } else if (a[j] == 1) {
+            j++;
+        } else {
+            swap(a[j], a[k]);
+            k--;
+        }
+    }
+}
